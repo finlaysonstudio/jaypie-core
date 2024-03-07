dee0bb9224b5854e5497a995c462bfb775985e83
Adam Finlayson <amf@adamfinlayson.com>
2024-03-06 19:37:18 -0600

    chore: config: eslint import and vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,10 +19,29 @@ module.exports = {
         node: true,
       },
     },
+    {
+      files: ["*.cjs"],
+      rules: {
+        "import/no-commonjs": "off",
+      },
+    },
+    {
+      files: ["__tests__/**", "**/*.spec.js", "**/*.test.js"],
+      plugins: ["vitest"],
+      extends: ["plugin:vitest/recommended"],
+      rules: {
+        "vitest/no-focused-tests": "error",
+        "vitest/no-disabled-tests": "warn",
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["prettier"],
+  rules: {
+    "import/extensions": ["error", "ignorePackages"],
+    "import/no-commonjs": "error",
+  },
 };
