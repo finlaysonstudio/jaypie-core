5c049c9d9cb6066930ca3fd3b90d4019fa2b0390
Adam Finlayson <amf@adamfinlayson.com>
2024-03-08 12:45:42 -0600

    refactor: unwrap

--- src/__mocks__/jaypieHandler.module.js
+++ src/__mocks__/jaypieHandler.module.js
@@ -4,9 +4,7 @@ function init() {
   // Simulate the module
   // eslint-disable-next-line no-unused-vars
   const module = vi.fn((handler, options) => {
-    return vi.fn((...args) => {
-      return handler(...args);
-    });
+    return (...args) => handler(...args);
   });
 
   // return the module
