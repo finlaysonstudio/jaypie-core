26b5249aa915234f0bf1da5ecd07f1cdecbc57a7
Adam Finlayson <amf@adamfinlayson.com>
2024-03-08 08:30:35 -0600

    feat: init: knowdev lib module

---
+++ src/core/__tests__/knowdev.lib.spec.js
@@ -0,0 +1,101 @@
+// eslint-disable-next-line no-unused-vars
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+// Subject
+import {
+  cloneDeep,
+  envBoolean,
+  force,
+  getHeaderFrom,
+  HTTP,
+  LOG,
+  log,
+  Logger,
+  moduleLogger,
+  // eslint-disable-next-line import/named
+  NAME,
+  ProjectError,
+  placeholders,
+  silent,
+  validate,
+  VALIDATE,
+} from "../knowdev.lib.js";
+
+//
+//
+// Mock constants
+//
+
+//
+//
+// Mock modules
+//
+
+//
+//
+// Mock environment
+//
+
+const DEFAULT_ENV = process.env;
+beforeEach(() => {
+  process.env = { ...process.env };
+});
+afterEach(() => {
+  process.env = DEFAULT_ENV;
+});
+
+//
+//
+// Run tests
+//
+
+describe("KnowDev Lib", () => {
+  describe("Constants", () => {
+    it("Exposes constants", () => {
+      expect(HTTP).toBeObject();
+      expect(LOG).toBeObject();
+      expect(VALIDATE).toBeObject();
+    });
+  });
+  describe("Errors", () => {
+    it("Exposes ProjectError", () => {
+      expect(ProjectError).toBeFunction();
+    });
+    it("Does not expose NAME", () => {
+      expect(NAME).toBeUndefined();
+      // If it does expose name:
+      //   - Delete this test if you want to expose NAME, especially if it is not "ProjectError"
+      //   - If you did not want to expose name and it is ProjectError, now is the time to fix it
+    });
+  });
+  describe("Functions", () => {
+    it("Exposes envBoolean, getHeaderFrom, and placeholders", () => {
+      expect(cloneDeep).toBeFunction();
+      expect(envBoolean).toBeFunction();
+      expect(force).toBeFunction();
+      expect(getHeaderFrom).toBeFunction();
+      expect(placeholders).toBeFunction();
+      expect(validate).toBeFunction();
+    });
+    it("cloneDeep works as expected", () => {
+      const obj = { a: { b: { c: 1 } } };
+      const clone = cloneDeep(obj);
+      expect(clone).not.toBe(obj);
+      expect(clone).toEqual(obj);
+    });
+  });
+  describe("Logging", () => {
+    it("Exposes log, Logger, moduleLogger, and silent", () => {
+      expect(log).toBeObject();
+      expect(Logger).toBeFunction();
+      expect(moduleLogger).toBeObject();
+      expect(silent).toBeObject();
+    });
+    it("Logger can be instantiated", () => {
+      const logger = new Logger();
+      expect(logger).toBeObject();
+      expect(logger).toBeInstanceOf(Logger);
+      expect(logger.trace).toBeFunction();
+    });
+  });
+});
---
+++ src/core/knowdev.lib.js
@@ -0,0 +1,57 @@
+import validate from "@knowdev/arguments";
+import HTTP from "@knowdev/http";
+import log from "@knowdev/log";
+
+//
+//
+// Export
+//
+
+// Constants
+const VALIDATE = validate.TYPE;
+
+export { HTTP, VALIDATE };
+export const LOG = {
+  FORMAT: log.LOG_FORMAT,
+  LEVEL: log.LOG_LEVEL,
+};
+
+// Functions
+const force = validate.force;
+const getHeaderFrom = HTTP.getHeaderFrom;
+export { force, getHeaderFrom, validate };
+export { envBoolean, placeholders } from "@knowdev/functions";
+export { default as cloneDeep } from "lodash.clonedeep";
+
+// Errors
+export {
+  BadGatewayError,
+  BadRequestError,
+  ConfigurationError,
+  ERROR,
+  ForbiddenError,
+  GatewayTimeoutError,
+  GoneError,
+  IllogicalError,
+  InternalError,
+  MethodNotAllowedError,
+  MultiError,
+  // NAME, // Do not export NAME
+  NotFoundError,
+  NotImplementedError,
+  ProjectError,
+  ProjectMultiError,
+  RejectedError,
+  TeapotError,
+  UnauthorizedError,
+  UnavailableError,
+  UnhandledError,
+  UnreachableCodeError,
+} from "@knowdev/errors";
+
+// Logging
+const Logger = log.Logger;
+const moduleLogger = log.moduleLogger;
+const silent = log.silent;
+
+export { log, Logger, moduleLogger, silent };
