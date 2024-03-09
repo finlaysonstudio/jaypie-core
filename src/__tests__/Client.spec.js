06b501c4f5d19dca8004fddd3c2d645c4eed9a8d
Adam Finlayson <amf@adamfinlayson.com>
2024-03-08 22:11:56 -0600

    test: client

--- src/__tests__/Client.spec.js
+++ src/__tests__/Client.spec.js
@@ -7,6 +7,8 @@ import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 // Subject
 import { lambdaHandler, log } from "../index.js";
+import { createLogWith, moduleLogger } from "../core.js";
+import { restoreLog, spyLog } from "../../test/mockLog.js";
 
 //
 //
@@ -18,6 +20,15 @@ import { lambdaHandler, log } from "../index.js";
 // Mock modules
 //
 
+vi.mock("../core.js", async () => {
+  const actual = await vi.importActual("../core.js");
+  const module = {
+    ...actual,
+    createLogWith: vi.fn((tags) => actual.exportedLog.with(tags)),
+  };
+  return module;
+});
+
 //
 //
 // Mock environment
@@ -26,9 +37,14 @@ import { lambdaHandler, log } from "../index.js";
 const DEFAULT_ENV = process.env;
 beforeEach(() => {
   process.env = { ...process.env };
+  spyLog(log);
+  spyLog(moduleLogger);
 });
 afterEach(() => {
   process.env = DEFAULT_ENV;
+  restoreLog(log);
+  restoreLog(moduleLogger);
+  vi.resetAllMocks();
 });
 
 //
@@ -38,17 +54,31 @@ afterEach(() => {
 
 describe("Client", () => {
   describe("Lambda Handler", () => {
-    it("Calls a function I provide it", () => {
+    it("Calls a function I provide it", async () => {
       // Arrange
       const mockFunction = vi.fn();
       const handler = lambdaHandler(mockFunction);
       const args = [1, 2, 3];
       // Act
-      handler(...args);
+      await handler(...args);
       // Assert
       expect(mockFunction).toHaveBeenCalledTimes(1);
       expect(mockFunction).toHaveBeenCalledWith(...args);
     });
-    it.todo("Provides the handler a logger with module:lambda");
+    it("Creates a logger with module:lambda", async () => {
+      // Arrange
+      const handler = lambdaHandler(vi.fn());
+      // Act
+      await handler();
+      // Assert
+      expect(createLogWith).toHaveBeenCalledTimes(1);
+      expect(createLogWith).toHaveBeenCalledWith({ module: "lambdaHandler" });
+    });
+    it("Passes jaypieHandler its own log, not the one we pass it", async () => {
+      // Arrange
+      const handler = lambdaHandler(vi.fn());
+      // Act
+      await handler();
+    });
   });
 });
