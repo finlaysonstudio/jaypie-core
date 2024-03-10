6ad7461f45328c7e44735ee804d5352ce23bd37b
Adam Finlayson <amf@adamfinlayson.com>
2024-03-09 21:49:05 -0600

    chore: install: jest-json-schema

--- testSetup.js
+++ testSetup.js
@@ -1,3 +1,6 @@
 import { expect } from "vitest";
 import * as matchers from "jest-extended";
+import { matchers as jsonSchemaMatchers } from "jest-json-schema";
+
 expect.extend(matchers);
+expect.extend(jsonSchemaMatchers);
