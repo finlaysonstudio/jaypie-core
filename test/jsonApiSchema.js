0061329f34ae121dfa7f6bc7f5dbb8954b209303
Adam Finlayson <amf@adamfinlayson.com>
2024-03-09 21:49:42 -0600

    chore: magpie: json api schema

---
+++ test/jsonApiSchema.js
@@ -0,0 +1,40 @@
+// Compatible with jest-json-schema and ajv
+
+export const jsonApiSchema = {
+  type: "object",
+  properties: {
+    data: {
+      type: "object",
+      properties: {
+        id: { type: "string" },
+        type: { type: "string" },
+        attributes: { type: "object" },
+        links: { type: "object" },
+        meta: { type: "object" },
+        relationships: { type: "object" },
+      },
+      required: ["id", "type"],
+    },
+    meta: { type: "object" },
+  },
+  required: ["data"],
+};
+
+export const jsonApiErrorSchema = {
+  type: "object",
+  properties: {
+    errors: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          status: { type: "number" },
+          title: { type: "string" },
+          detail: { type: "string" },
+        },
+        required: ["status", "title"],
+      },
+    },
+  },
+  required: ["errors"],
+};
