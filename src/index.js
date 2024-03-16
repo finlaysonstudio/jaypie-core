2fdd8cc225dc9c7edf3803f10a3b75f4fdc93bf3
Adam Finlayson <amf@adamfinlayson.com>
2024-03-16 09:35:03 -0500

    chore: remove: lambda handler
    moved to @jaypie/lambda

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ export { CDK, JAYPIE, default, moduleLogger, PROJECT } from "./core.js";
 // Jaypie
 export { default as expressHandler } from "./expressHandler.module.js";
 export { default as jaypieHandler } from "./jaypieHandler.module.js";
-export { default as lambdaHandler } from "./lambdaHandler.module.js";
 
 // KnowDev
 export * from "./core/knowdev.lib.js";
