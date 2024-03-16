9ce7ec8da42671ca3fa2f0883813125d9589d1f5
Adam Finlayson <amf@adamfinlayson.com>
2024-03-15 20:17:29 -0500

    feat: apply log tags to module logger

--- src/core.js
+++ src/core.js
@@ -85,20 +85,20 @@ if (
     moduleLogger = new Logger({
       format: LOG.FORMAT.JSON,
       level: process.env.MODULE_LOG_LEVEL,
-      tags: { layer: JAYPIE.LAYER.MODULE },
+      tags: { ...logTags(), layer: JAYPIE.LAYER.MODULE },
     });
   } else {
     moduleLogger = new Logger({
       format: LOG.FORMAT.JSON,
       level: process.env.LOG_LEVEL,
-      tags: { layer: JAYPIE.LAYER.MODULE },
+      tags: { ...logTags(), layer: JAYPIE.LAYER.MODULE },
     });
   }
 } else {
   moduleLogger = new Logger({
     format: LOG.FORMAT.JSON,
     level: LOG.LEVEL.SILENT,
-    tags: { layer: JAYPIE.LAYER.MODULE },
+    tags: { ...logTags(), layer: JAYPIE.LAYER.MODULE },
   });
 }
 export { moduleLogger };
