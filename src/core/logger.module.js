89905537d8c13630c2a94dbe6c4134e487250025
Adam Finlayson <amf@adamfinlayson.com>
2024-03-18 22:17:54 -0500

    feat: update logger

--- src/core/logger.module.js
+++ src/core/logger.module.js
@@ -1,5 +1,7 @@
+import { Logger } from "@knowdev/log";
+
 import { JAYPIE } from "./constants.js";
-import { envBoolean, LOG, Logger } from "./knowdev.lib.js";
+import { envBoolean, LOG } from "./knowdev.lib.js";
 import logTags from "./logTags.function.js";
 
 //
@@ -11,14 +13,17 @@ class JaypieLogger {
   constructor({
     handler = JAYPIE.UNKNOWN,
     layer = JAYPIE.UNKNOWN,
+    level = process.env.LOG_LEVEL, // DEFAULT.LEVEL provided by Logger is debug
     logger = JAYPIE.UNKNOWN,
     ...tags
   } = {}) {
+    this.level = level;
+    // _NOT_ tagging `level`
     this._tags = { ...logTags(), handler, layer, logger, ...tags };
     this._logger = new Logger({
       format: LOG.FORMAT.JSON,
-      // level: process.env.LOG_LEVEL || DEFAULT.LEVEL, // Default provided by Logger
-      tags: this.tags,
+      level,
+      tags: this._tags,
     });
     this._loggers = [];
     this._loggers.push(this._logger);
@@ -64,16 +69,23 @@ class JaypieLogger {
     return this._logger.warn(...args);
   }
   with(...args) {
-    const logger = this._logger.with(...args);
+    const logger = new JaypieLogger({
+      ...this._tags,
+      level: this.level,
+      ...args,
+    });
     this._loggers.push(logger);
     return logger;
   }
 
   // Jaypie-specifics
 
-  module({ layer, lib, ...tags } = {}) {
-    const logger = new Logger({
-      format: LOG.FORMAT.JSON,
+  lib({ layer, lib, ...tags } = {}) {
+    const logger = new JaypieLogger({
+      ...this._tags,
+      layer,
+      lib,
+      ...tags,
       level: () => {
         if (process.env.MODULE_LOG_LEVEL) {
           return process.env.MODULE_LOG_LEVEL;
@@ -82,12 +94,6 @@ class JaypieLogger {
           return process.env.LOG_LEVEL;
         }
         return LOG.LEVEL.SILENT;
-      },
-      tags: {
-        ...this._tags,
-        layer,
-        lib,
-        ...tags,
       },
     });
     this._loggers.push(logger);
