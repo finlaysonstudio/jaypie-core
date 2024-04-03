ad91f5cbe4f236e565e8f8a20d8339c48a228931
Adam Finlayson <amf@adamfinlayson.com>
2024-04-03 15:16:45 -0400

    test: logger

--- src/__tests__/core.spec.js
+++ src/__tests__/core.spec.js
@@ -37,4 +37,17 @@ describe("Jaypie Core", () => {
     expect(JAYPIE).toBeObject();
     expect(PROJECT.SPONSOR.FINLAYSON).toBe("finlaysonstudio");
   });
+  it("Exports log", () => {
+    expect(log).toBeObject();
+    expect(log.debug).toBeFunction();
+    expect(log.error).toBeFunction();
+    expect(log.fatal).toBeFunction();
+    expect(log.info).toBeFunction();
+    expect(log.lib).toBeFunction();
+    expect(log.tag).toBeFunction();
+    expect(log.trace).toBeFunction();
+    expect(log.untag).toBeFunction();
+    expect(log.warn).toBeFunction();
+    expect(log.with).toBeFunction();
+  });
 });
