dd91b6bcf41972e27ebb26f92594fa2980b00bce
Adam Finlayson <amf@adamfinlayson.com>
2024-04-15 19:40:02 -0500

    feat: export arguments lib

---
+++ src/lib/__tests__/arguments.lib.spec.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+// Subject
+import argumentsLib from "../arguments.lib.js";
+import { force, TYPE } from "../arguments.lib.js";
+
+//
+//
+// Mock environment
+//
+
+const DEFAULT_ENV = process.env;
+beforeEach(() => {
+  process.env = { ...process.env };
+});
+afterEach(() => {
+  process.env = DEFAULT_ENV;
+});
+
+//
+//
+// Run tests
+//
+
+describe("Arguments Lib", () => {
+  it("Exports what we expect", () => {
+    expect(force).toBeFunction();
+    expect(TYPE).toBeObject();
+    expect(argumentsLib).toBeFunction();
+  });
+});
